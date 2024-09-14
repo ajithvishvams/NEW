00f0866164940da34baeadfe8e0b40b2d087c537
Ajithvishva M S <ajithvishvams19@gmail.com>
2024-09-14 18:22:14 +0530

    adding for loop

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class ForLoop {
+public class ForLoop {
+    public static void main(String args[]){
+        for(int i = 0; i <=4 ; i++){
+            System.out.println("Day "+ i);
+            for (int j = 0; j<= 7;  j++){
+                System.out.println(" "+ (j+9) + " - "  + (j+10));
+            }
+        }
+    }
 }
